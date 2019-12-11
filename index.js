c2ca3b2d18a24a7164da9d5f7258e5920b9dd5df
Danielle B <danibaxx@hotmail.com>
2019-12-10 19:06:44 -0700

    initial commit, pulled in dependencies and 3rd parties that I thought I might use, added 1st router to welcome.js, working

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require('express');
+const welcomeRouter = require('./routers/welcome');
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/', welcomeRouter)
+
+server.listen(3000, () => {
+  console.log('\n*** Server is running http://localhost:3000 ***\n')
+})
